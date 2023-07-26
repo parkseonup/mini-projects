6a5b7b85c4be6e2eacf22ea0bb9bff780e2faf2d
Seonu <seonup95@gmail.com>
2023-07-26 13:02:19 +0900

    feat: Create Calendar, Datepicker page

---
+++ src/pages/calendar.tsx
@@ -0,0 +1,7 @@
+export default function Calendar() {
+  return (
+    <>
+      <h2>Calendar</h2>
+    </>
+  );
+}
---
+++ src/pages/datePicker.tsx
@@ -0,0 +1,7 @@
+export default function DatePicker() {
+  return (
+    <>
+      <h2>Date Picker</h2>
+    </>
+  );
+}
